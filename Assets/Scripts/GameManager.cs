4084cd682d3e8217c46ca28f4cde8c9a0b256354
ElSachs <75885665+ElSachs@users.noreply.github.com>
2023-11-11 13:20:54 +0100

    gameManager Update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public Player[] players;
-    
+    private Player activePlayer;
+    private int currentIndex;
+
+    public void SetNextPlayer()
+    {
+        currentIndex = currentIndex >= players.Length - 1 ? 0 : currentIndex+1;
+        Debug.Log(players.Length);
+        Debug.Log(currentIndex);
+        activePlayer = players[currentIndex];
+    }
+
+    public Player GetActivePlayer()
+    {
+        return activePlayer;
+    }
     
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private int astro;
+    private int playerIndex;
+    private Color color;
+    private Sprite sprite;
+    private int playerScore;
+
+    public int GetIndex()
+    {
+        return playerIndex;
+    }
+
+    public void SetIndex(int index)
+    {
+        playerIndex = index;
+    }
+
+    public int GetScore()
+    {
+        return playerScore;
+    }
+
+    public void SetScore(int score)
+    {
+        playerScore += score;
+    }
+
+    public void ResetScore()
+    {
+        playerScore = 0;
+    }
+    
+    
 }
