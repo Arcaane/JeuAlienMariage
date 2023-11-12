297bbebc33315be5c7abe9eff059687644148a40
MARTY Grégoire <g.marty@rubika-edu.com>
2023-11-12 05:27:59 +0100

    Inté Sprite Perso / Change SO Question

--- Assets/Scripts/Date.cs
+++ Assets/Scripts/Date.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 [Serializable]
 public class Date
 {
     public List<QuestionsSO> questions;
+    public List<Sprite> spritesAliens;
 }
